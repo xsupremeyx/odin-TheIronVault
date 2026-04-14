8504ce435ef55e28ae7f6b6662ba2463742c95c6
Yash <86472828+xsupremeyx@users.noreply.github.com>
2026-04-14 15:16:11 +0000

    feat: init commit with basic setup.

--- app.js
+++ app.js
@@ -1,33 +1,46 @@
+// express imports
 const express = require("express");
 const app = express();
 const path = require("node:path");
+require("dotenv").config();
+
+// ejs and static files setup
 app.set("views", path.join(__dirname,"views"));
 app.set("view engine", "ejs");
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
 
+// middleware for form data parsing
+app.use(express.urlencoded({ extended: true }));
+
 // import routes
 const indexRouter = require("./routes/indexRouter");
+const categoryRouter = require("./routes/categoryRouter");
+const itemRouter = require("./routes/itemRouter");
 
-// define routes
+// use routes
 app.use("/", indexRouter);
+app.use("/categories", categoryRouter);
+app.use("/items", itemRouter);
 
 // 404 handler
 app.use((req, res, next) => {
-    res.status(404).send("404 Not Found");
+    const err = new Error("Not Found");
+    err.status = 404;
+    next(err);
 })
 
 // global error handler
 app.use((error, req, res, next) => {
     console.error(error);
-    res.status(500).send("Internal Server Error");
+    res.status(error.status || 500).send(error.message || "Internal Server Error");
 })
 
-const PATH = process.env.PORT || 3000
-app.listen(PATH, (err) => {
+const PORT = process.env.PORT || 3000
+app.listen(PORT, (err) => {
     if(err){
         console.error("Error starting server:", err);
         return;
     }
-    console.log(`Server is running on port ${PATH}`);
+    console.log(`Server is running on port ${PORT}`);
 })
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,10 +2,12 @@ const { Router } = require("express");
 const indexRouter = Router();
 
 // import controllers
-const indexController = require("../controllers/indexController");
+// const indexController = require("../controllers/indexController");
 
 // define routes
 
-indexRouter.get("/", indexController.getIndex);
+indexRouter.get("/", (req, res) => {
+    res.render("index");
+})
 
 module.exports = indexRouter;
